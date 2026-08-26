7134a7c6773c1f41f6400d2e30751cd58306f81f
felipebalanar-collab <felipebalanar-collab@users.noreply.github.com>
2026-08-26 22:37:54 +0000

    deploy: 07051bdd39579bcdf00b887a6a3a40f9faf608f0

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tienda-galeria-electronica/sw.js', { scope: '/tienda-galeria-electronica/' })})}
